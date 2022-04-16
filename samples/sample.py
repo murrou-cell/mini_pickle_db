25fe7df88935f1e9a2d81292b5cdaca1636403e3
marinABdump <97778128+marinABdump@users.noreply.github.com>
2022-04-16 21:48:57 +0300

    .

---
+++ samples/sample.py
@@ -0,0 +1,37 @@
+from pickle_wrapper import p_wrapper
+import os 
+
+db = p_wrapper()
+
+db_file_path = 'db'
+
+db_file_name = 'simple_db.json'
+
+db.db_file = os.path.join(db_file_path, db_file_name)
+
+single_doc = {
+    'test_1': 1,
+    'test_2': 2
+}
+
+db.insert(single_doc)
+
+print(db.load())
+
+multiple_docs = [
+    {
+        'test_1': 1,
+        'test_2': 2
+    },
+    {
+        'test_3': 3,
+        'test_4': 4
+    }
+]
+db.insert(multiple_docs)
+
+print(db.load())
+
+print(db.query_one({'test_2': 2}))
+
+print(db.query_many({'test_2': 2}))
